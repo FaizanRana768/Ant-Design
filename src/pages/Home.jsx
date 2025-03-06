ca25572fba5a2bfd846f83b20f0e5d76711ae9d8
FaizanRana768 <FaizanAli.mentorsol@gmail.com>
2025-03-06 15:47:22 +0500

    use the antd modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Button, Modal } from 'antd';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div>
-      Home
+      <h3>Home</h3>
+      <Button type="primary" onClick={showModal}>Click Me</Button>
+      <Modal title="Modal Title" open={isModalVisible} onCancel={handleCancel} footer={null}>
+        <p>Modal content goes here...</p>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
