3a7339a6c754a562ce688598acd0921beae5598d
FaizanRana768 <FaizanAli.mentorsol@gmail.com>
2025-03-06 13:08:56 +0500

    make the about page

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,12 +2,13 @@ import { Layout } from "antd";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import AboutUs from "../pages/AboutUs";
 
 const { Content } = Layout;
 
 const routes = [
   { path: "/", element: <Home /> },
-  { path: "/about", element: <h2>About Page</h2> },
+  { path: "/about", element: <AboutUs /> },
   { path: "/gallery", element: <h2>Gallery Page</h2> },
   { path: "/contact", element: <h2>Contact Page</h2> },
   { path: "/blog", element: <h2>Blog Page</h2> },
